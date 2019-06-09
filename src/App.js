6b8afbdf943fd711d01344b5606849ded9ea7f73
edim dendy <edim@softwareseni.com>
2019-06-10 06:07:46 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // pages
 import DefaultLayouts from './components/layout/default-layouts'
 import Products from './pages/products';
@@ -9,13 +10,15 @@ import AddProduct from './pages/add-product';
 class App extends React.Component {
   render() {
     return (
-      <DefaultLayouts>
-        <Switch>
-          <Route path="/" component={Products} exact />
-          <Route path="/detail/:id" component={ProductDetail} exact />
-          <Route path="/add" component={AddProduct} exact />
-        </Switch>
-      </DefaultLayouts>
+      <BrowserRouter>
+        <DefaultLayouts>
+          <Switch>
+            <Route path="/" component={Products} exact />
+            <Route path="/detail/:id" component={ProductDetail} exact />
+            <Route path="/add" component={AddProduct} exact />
+          </Switch>
+        </DefaultLayouts>
+      </BrowserRouter>
     );
   }
 }
